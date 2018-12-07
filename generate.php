9e7513255e7784fc4ef629950e460e3f7f24c421
Mirko Pira <30497555+mirkodevx@users.noreply.github.com>
2018-12-07 16:03:23 +0100

    Generate Fix

--- generate.php
+++ generate.php
@@ -6,7 +6,7 @@ if (file_exists(__DIR__.'/vendor/autoload.php')) {
 } elseif(file_exists(__DIR__.'/../../autoload.php')) {
     require __DIR__.'/../../autoload.php';
 }else{
-
+    die('DIE');
 }
 
 $application = new Spatie\SchemaOrg\Generator\Console\Application();
